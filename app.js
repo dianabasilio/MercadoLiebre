cf8ce64b60e652e1632029f4471573931f145556
dianabasilio <dianabasilio99@gmail.com>
2021-08-17 20:09:54 -0500

    dando estilos, acomodando formato

--- app.js
+++ app.js
@@ -16,8 +16,23 @@ app.listen(3000, ()=> {
     console.log('Server running at port 3000');
 });
 
+
 app.get('/', (req,res) =>{
     //res se encuentra en el callback en el segundo parámetro
     res.sendFile(path.resolve('./views/home.html'))
+
+});
+
+app.get('/register', (req,res) =>{
+    //res se encuentra en el callback en el segundo parámetro
+    res.sendFile(path.resolve('./views/register.html'))
+    //res.sendFile(__dirname + '/views/register.html');
+
+});
+
+app.get('/login', (req,res) =>{
+    //res se encuentra en el callback en el segundo parámetro
+    res.sendFile(path.resolve('./views/login.html'))
+    //res.sendFile(__dirname + '/views/login.html');
 
 });
